eb8dc224edeb33fe81e250b37d5eb125de99a25c
Aziz Gasimov <aziz.gasimov94@gmail.com>
2020-11-12 23:58:22 +0100

    footer component and bootstrap added

---
+++ practice-app/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="footer">
+            Created by Aziz Gasimov on 12/11/20.
+            Copyright © 2020 . All rights reserved.
+        </div>
+    );
+}
+
+export default Footer;
+
